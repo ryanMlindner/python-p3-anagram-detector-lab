9f2c0f4f10fe05bf46bbafe68624d1cf3fc2ed06
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-07-06 14:14:06 -0600

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        anagrams = []
+        compare = sorted([letter for letter in self.word])
+        for word in list:
+            sorted_word = sorted([letter for letter in word])
+            if (compare == sorted_word):
+                anagrams.append(word)
+        return anagrams
